080fb7242114953856898d99aa7842ae20404180
Beto Esquivel <ppbeto94@gmail.com>
2015-11-11 17:12:39 +0000

    Finishes part 1 of the assignment. Identifies all errors made by qTag on the pretokenized text. Apparently QTag has troubles differencing between nouns and verbs, plus, it doesn't have any tag for currency symbols, which might be very useful.

--- perl_tokenizer.pl
+++ perl_tokenizer.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 use utf8;
+binmode STDOUT, ":utf8";
+use open IN => ":encoding(utf8)", OUT => ":utf8";
 
 open BR, "< Part1Text.txt" or die "can't open input file right now";
 
@@ -10,8 +12,8 @@ while (<BR>) {
   # Only punctuation that is not part of a word is a token on its own.
   # This only works on this text, because one flaw it has is decimal numbers
   # and currency.
-  s/([,"$%\^\&*()+=\[\]!?])/ $1 /g;
-  s/([^A-Z.])(\.)\s+/$1 $2 /g;
+  s/(["\p{Sc}%\^\&*()+=\[\]!?])/ $1 /g;
+  s/([^A-Z.\p{Sc}])([.,])\s+/$1 $2 /g;
   s/\s+/\n/g;
   print $_;
 }
